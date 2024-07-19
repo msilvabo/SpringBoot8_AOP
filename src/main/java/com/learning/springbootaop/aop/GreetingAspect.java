f17a74bc04dcd3acce46c92884d5ba7bbf8547b4
Moises Silva Cardenas <moises.silva@et.bo>
2024-07-19 16:48:15 -0400

    109. Advice Around

--- src/main/java/com/learning/springbootaop/aop/GreetingAspect.java
+++ src/main/java/com/learning/springbootaop/aop/GreetingAspect.java
@@ -1,6 +1,7 @@
 package com.learning.springbootaop.aop;
 
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -13,37 +14,47 @@ import java.util.Arrays;
 public class GreetingAspect {
     private Logger logger = LoggerFactory.getLogger(this.getClass());
 
-    @Before("execution(* com.learning.springbootaop.services.GreetingService.sayHello(..))")
+    @Before("execution(* com.learning.springbootaop.services.GreetingService.*(..))")
 //    @Before("execution(* com.learning.springbootaop..*.*(..))")
     public void loggerBefore(JoinPoint joinPoint) {
         String method = joinPoint.getSignature().getName();
         String args = Arrays.toString(joinPoint.getArgs());
-        logger.info("Antes ...." + method + " con los argumentos  .. " + args);
+        logger.info("Before ...." + method + " con los argumentos  .. " + args);
     }
-    @After("execution(* com.learning.springbootaop.services.GreetingService.sayHello(..))")
+    @After("execution(* com.learning.springbootaop.services.GreetingService.*(..))")
     public void loggerAfter(JoinPoint joinPoint) {
         String method = joinPoint.getSignature().getName();
         String args = Arrays.toString(joinPoint.getArgs());
-        logger.info("Despues ...." + method + " con los argumentos  .. " + args);
+        logger.info("After ...." + method + " con los argumentos  .. " + args);
     }
 
-//    @Around("execution(* com.learning.springbootaop.services.GreetingService.sayHello(..))")
-//    public void loggerAround(JoinPoint joinPoint) {
-//        String method = joinPoint.getSignature().getName();
-//        String args = Arrays.toString(joinPoint.getArgs());
-//        logger.info("Around ...." + method + " con los argumentos  .. " + args);
-//    }
-
-    @AfterReturning("execution(* com.learning.springbootaop.services.GreetingService.sayHello(..))")
+    @AfterReturning("execution(* com.learning.springbootaop.services.GreetingService.*(..))")
     public void AfterReturning(JoinPoint joinPoint) {
         String method = joinPoint.getSignature().getName();
         String args = Arrays.toString(joinPoint.getArgs());
-        logger.info("Despues Retorno ...." + method + " con los argumentos  .. " + args);
+        logger.info("AfterReturning ...." + method + " con los argumentos  .. " + args);
     }
     @AfterThrowing("execution(* com.learning.springbootaop.services.GreetingService.*(..))")
     public void AfterThrowing(JoinPoint joinPoint) {
         String method = joinPoint.getSignature().getName();
         String args = Arrays.toString(joinPoint.getArgs());
         logger.info("AfterThrowing ...." + method + " con los argumentos  .. " + args);
+    }
+
+    @Around("execution(* com.learning.springbootaop.services.*.*(..))")
+    public Object loggerAround(ProceedingJoinPoint joinPoint) throws Throwable {
+        String method = joinPoint.getSignature().getName();
+        String args = Arrays.toString(joinPoint.getArgs());
+
+        Object result = null;
+        try {
+            logger.info("Around ...." + method + "() con los argumentos  .. " + args);
+            result = joinPoint.proceed();
+            logger.info("Around ...." + method + "() con los argumentos  .. " + args + " ---  Resultado: " + result);
+            return result;
+        } catch (Throwable e) {
+            logger.error("Error en la llamada del metodo " + method + "()");
+            throw e;
+        }
     }
 }
