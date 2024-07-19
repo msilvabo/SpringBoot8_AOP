dfc4862e107228ef4cef9cf2046d24aafdac0e7d
Moises Silva Cardenas <moises.silva@et.bo>
2024-07-19 11:56:56 -0400

    105. Añadiendo componente Service

--- src/main/java/com/learning/springbootaop/controllers/GreetingController.java
+++ src/main/java/com/learning/springbootaop/controllers/GreetingController.java
@@ -1,5 +1,7 @@
 package com.learning.springbootaop.controllers;
 
+import com.learning.springbootaop.services.GreetingService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,8 +13,11 @@ import java.util.Collections;
 @RequestMapping("/app")
 public class GreetingController {
 
+    @Autowired
+    private GreetingService greetingService;
+
     @GetMapping("/greeting")
     public ResponseEntity greeting(){
-        return ResponseEntity.ok(Collections.singletonMap("greeting", "Hello World"));
+        return ResponseEntity.ok(Collections.singletonMap("greeting", greetingService.sayHello("Pepito",", Hola don Jose")));
     }
 }
---
+++ src/main/java/com/learning/springbootaop/services/GreetingService.java
@@ -0,0 +1,8 @@
+package com.learning.springbootaop.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public interface GreetingService {
+    String sayHello(String person, String greeting);
+}
