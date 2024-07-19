b4af9a3449c6ef82f0ba4a5528f30d0ba25a3769
Moises Silva Cardenas <moises.silva@et.bo>
2024-07-19 17:08:06 -0400

    110. Ordenando los Aspectos con @Order

---
+++ src/main/java/com/learning/springbootaop/aop/GreetingFooAspect.java
@@ -0,0 +1,32 @@
+package com.learning.springbootaop.aop;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+
+@Order(1)
+@Aspect
+@Component
+public class GreetingFooAspect {
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @Before("execution(* com.learning.springbootaop.services.GreetingService.*(..))")
+    public void loggerBefore(JoinPoint joinPoint) {
+        String method = joinPoint.getSignature().getName();
+        String args = Arrays.toString(joinPoint.getArgs());
+        logger.info("Before GreetingFOO...." + method + " con los argumentos  .. " + args);
+    }
+    @After("execution(* com.learning.springbootaop.services.GreetingService.*(..))")
+    public void loggerAfter(JoinPoint joinPoint) {
+        String method = joinPoint.getSignature().getName();
+        String args = Arrays.toString(joinPoint.getArgs());
+        logger.info("After GreetingFOO ...." + method + " con los argumentos  .. " + args);
+    }
+}
